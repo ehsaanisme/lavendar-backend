6494b032dd3fec4ab50001fde50165e406a82b3d
ehsaanisme <ehsaanismail@gmail.com>
2025-03-01 11:59:03 -0800

    Added GPT API Key and ENV setup

--- src/config/env.js
+++ src/config/env.js
@@ -8,12 +8,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const projectRoot = join(__dirname, '../../');
 
-// Imported instance of .env variables
 const env = {
     corsOrigin: (!process.env.CORS_ORIGIN || process.env.CORS_ORIGIN === 'false' || process.env.CORS_ORIGIN === 'null') ?
         false : process.env.CORS_ORIGIN, // Sets corsOrigin to false (disables cors) if requested / none provided
-    port: process.env.PORT || 3000, // uses the port in .env or defaults to 3000
+    port: process.env.PORT || 3000, // Uses the port in .env or defaults to 3000
     rootLocation: projectRoot,
-}
+    openaiApiKey: process.env.OPENAI_API_KEY || '', // OpenAI API Key from .env
+};
 
 export default env;
